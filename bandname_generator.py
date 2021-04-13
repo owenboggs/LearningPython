4bf87f1c1ecbf5fdfcfdf72bcaef01460a0c716b
NinjaLooter7 <exerciseninja@gmail.com>
2021-04-13 14:17:32 -0700

    Basic program to combine two inputs to create a bandname

---
+++ bandname_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator.")
+city_name = input("What city were you born in?")
+pet_name = input("What was the name of your first pet?")
+print(f"Your band name is {city_name}-{pet_name}!")
