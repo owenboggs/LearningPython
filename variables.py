2ff7ecda5612e94eb18f54bbbc6249a1ee554e97
NinjaLooter7 <exerciseninja@gmail.com>
2021-04-13 01:27:56 -0700

    Learning different types of variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+name = "jennifer"
+print(name)
+length = len(name)
+print(f"{name} is {length} long.")
+
+number1 = 5
+number2 = 7
+print(number1 + number2)
+
+floating_point_number = float(10)
+print(floating_point_number)
