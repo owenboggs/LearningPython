496ba9aeeedcc96453abede14241b8f795e435a8
NinjaLooter7 <exerciseninja@gmail.com>
2021-04-13 18:02:14 -0700

    different ways to manipulate numbers, rounding up and rounding down and more

---
+++ number_manipulation.py
@@ -0,0 +1,19 @@
+print(8 / 3)
+# returns 2.6666666666666665
+
+print(round(8 / 3))
+# round function returns the nearest whole number
+
+print(round(2.6666666666666665, 2))
+# ,2 rounds to 2 decimal places
+
+print(8 // 3)
+# floor division rounds down
+
+score = 7
+
+score += 1 # is the same as typing score = score + 1
+# this can be done using any operator
+
+print(score)
+
