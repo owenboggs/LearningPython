ef8b0635778c4a7816a7035941926eff9c09d376
NinjaLooter7 <exerciseninja@gmail.com>
2021-04-13 17:19:43 -0700

    mathematical_operations and order of operation

---
+++ mathematical_operations.py
@@ -0,0 +1,18 @@
+# order of operation is () ** * / + -
+# Parentheses
+# Exponents
+# Multiplication and Division (from left to right)
+# Addition and Subtraction (from left to right)
+
+print(3 * 3 + 3 / 3 - 3)
+# 3 * 3 = 9
+# 3 / 3 = 1
+# 9 + 1 = 10
+# 10 - 3 = 7
+
+print(3 * (3 + 3) / 3 - 3)
+# parentheses first
+# 3 + 3 = 6
+# 3 * 6 = 18
+# 18 / 3 = 6
+# 6 - 3 = 3
